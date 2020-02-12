54f028e93570d674f47fb5e7725721d2e5e5c8ec
Renzo <rmolivares@renzo-olivares.dev>
2020-02-11 19:54:13 -0800

    Implement more parser logic * Add leading and trailing whitespace trim function * Build BasicCommands and Connectors from parsed input * Member variable queue to store parsed commands

--- header/Parser.hpp
+++ header/Parser.hpp
@@ -1,14 +1,22 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
+#include "../header/BasicCommand.hpp"
+#include "../header/Connector.hpp"
+
 #include <iostream>
+#include <queue>
 #include <boost/regex.hpp>
+#include <boost/algorithm/string/trim.hpp>
 
 class Parser{
     private:
+        std::queue<Command*> parsedCmds;
     public:
         Parser();
         void run();
+        std::queue<Command*> getParsedCmds();
+        char* whitespaceTrimLt(std::string rawString);
 };
 
 #endif /* PARSER_HPP */
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -3,36 +3,50 @@
 Parser::Parser(){
 }
 void Parser::run(){
-    std::string sp("echo hello && echo goodbye ; mkdir \"bye\" &&echo             'lol' ||     echo fail ; echo 123; rshell"); 
+    //std::string sp("echo hello && echo goodbye ; mkdir \"bye\" &&echo             'lol' ||     echo fail ; echo 123; rshell"); 
+    std::string sp("echa A && echo B");
     boost::regex re("(^&&|^\\|\\||^;|^\\s+)?(?<executable>\\w+)(?<arguments>\\s+[\"']?\\w+[\"']?)?\\s?+(?<connector>&&|\\|\\||;)?"); 
     boost::sregex_iterator iter(sp.begin(), sp.end(), re);
     boost::sregex_iterator end;
 
     for( ; iter != end; ++iter ) {
-        /*
-        char* full = trim((*iter)[0]);
-        char* executable = trim((*iter)[2]);
-        char* arguments = trim((*iter)[3]);
-        char* connector = trim((*iter)[4]);
+        char* full = whitespaceTrimLt((*iter)[0]);
+        char* executable = whitespaceTrimLt((*iter)[2]);
+        char* arguments = whitespaceTrimLt((*iter)[3]);
+        char* connector = whitespaceTrimLt((*iter)[4]);
+
         Command* newCmd = new BasicCommand(executable, arguments);
         Command* newConnector = new Connector(connector);
+        std::cout << "Path: " << newCmd->getPath() << std::endl;
+        std::cout << "Args: " << newCmd->getArgs() << std::endl;
+        std::cout << "Connector: " << newConnector->cmdString() << std::endl;
+        parsedCmds.push(newCmd);
+        parsedCmds.push(newConnector);
         //push command into vector, then connector
-        */
-        std::cout << "full command: " << (*iter)[0] << '\n';
-        std::cout << "executable: " << (*iter)[2] << '\n';
-        std::cout << "arguments: " << (*iter)[3] <<'\n';
-        std::cout << "connector: " << (*iter)[4] <<'\n';
+        /*
+        std::cout << "full command: " << full << '\n';
+        std::cout << "executable: " << executable << '\n';
+        std::cout << "arguments: " << arguments <<'\n';
+        std::cout << "connector: " << connector <<'\n';
         std::cout << std::endl;
+        */
     }
     //return command vector
 }
 
-/*
-type Parser::whitespaceTrim(){
-    return trimmedstring
-    //trim leading and trailing whitespaces
+std::queue<Command*> Parser::getParsedCmds(){
+    return parsedCmds;
 }
 
+char* Parser::whitespaceTrimLt(std::string rawString){
+    boost::algorithm::trim(rawString);
+    int n = rawString.size();
+    char* outp = new char[n + 1];
+    std::copy(rawString.begin(), rawString.end(), outp);
+    outp[n] = '\0';
+    return outp;
+}
+/*
 type Parser::commentTrim(){
     return trimmedstring
     //trim the hashtag(not inside quotes) and everything after it

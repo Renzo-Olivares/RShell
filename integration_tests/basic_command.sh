78978333880c083740ada79b6b244f22a8a2daa3
phylchen <pchen080@ucr.edu>
2020-02-13 11:40:27 -0800

    integration tests for exit and basic command (single command)

---
+++ integration_tests/basic_command.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ls -a
+echo "hello"
+echo hello
+mkdir test
+echo world
+mkdir test2
+echa wrong
+ls
+rm -r test2
+ls
+echo cs100
+ls -a -l
+;
+&&
+||
+exit
---
+++ integration_tests/exit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+exit
+
+echo a; exit
+
+ls && exit
+
+mkdir test; exit
+
+ls -a || echo q && exit

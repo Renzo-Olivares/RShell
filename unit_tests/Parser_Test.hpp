7923b1d301193391d87f5cf314973de77fb9fa8a
Renzo <rmolivares@renzo-olivares.dev>
2020-02-13 22:48:49 -0800

    Add unit tests for Parser

--- unit_tests/Parser_Test.hpp
+++ unit_tests/Parser_Test.hpp
@@ -3,12 +3,31 @@
 
 #include "gtest/gtest.h"
 #include "../header/Parser.hpp"
+#include "../header/Command.hpp"
 
-/*
-TEST(ParserTest, BCPath){
 
-	EXPECT_EQ();
+TEST(ParserTest, SimpleCmd){
+    std::string testInput = "echo hello";
+    Parser* analyzer = new Parser(testInput);
+    analyzer->run();
+    std::queue<Command*> parsedCmds = analyzer->getParsedCmds();
+    std::cout << (parsedCmds.front())->cmdString() << std::endl;
+    Command* newCmd = parsedCmds.front();
+	EXPECT_EQ(std::string(newCmd->getPath()), "echo");
+	EXPECT_EQ(std::string((newCmd->getArgs())[1]), "hello");
 }
-*/
+
+TEST(ParserTest, WhiteSpaceTrim){
+    std::string test = "     echo      ";
+    Parser* analyzer = new Parser(test);
+    char* clean  = analyzer->whitespaceTrimLt(test);
+    EXPECT_EQ(std::string(clean), "echo");
+}
+
+TEST(ParserTest, CommentTrim){
+    std::string test = "echo hello# && echo goodbye";
+    Parser* analyzer = new Parser(test);
+    EXPECT_EQ(analyzer->commentTrim(test), "echo hello");
+}
 
 #endif //__PARSER_TEST_HPP__

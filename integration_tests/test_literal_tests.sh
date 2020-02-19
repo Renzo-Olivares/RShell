5db21e0b8fb5a6cf604d636b55ea6cc0ec535cba
Renzo <rmolivares@renzo-olivares.dev>
2020-02-19 15:17:54 -0800

    Add integration tests for literal and symbolic test command

---
+++ integration_tests/test_literal_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Testing literal test commands"
+
+echo "loading literal test commands"
+../rshell < "literaltest.txt"
+echo
+
+echo "literal test command integration test complete"
---
+++ integration_tests/test_symbolic_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Testing symbolic test commands"
+
+echo "loading symbolic test commands"
+../rshell < "symbolictest.txt"
+echo
+
+echo "symbolic test command integration test complete"

3f3134a6b1e3dc98f9af1a4d3e9aa9624acf8c5b
Renzo <rmolivares@renzo-olivares.dev>
2020-02-27 10:22:03 -0800

    update Parser unit tests

--- unit_tests/Parser_Test.hpp
+++ unit_tests/Parser_Test.hpp
@@ -4,8 +4,11 @@
 #include "gtest/gtest.h"
 #include "../header/Parser.hpp"
 #include "../header/Command.hpp"
+#include "../header/BasicCommand.hpp"
+#include "../header/Connector.hpp"
 #include <iostream>
 #include <string>
+#include <queue>
 
 
 TEST(ParserTest, SimpleCmd){
@@ -58,6 +61,50 @@ TEST(ParserTest, PopQuotesErrorDouble){
     std::string test = "\"echo hello";
     Parser* analyzer = new Parser("null");
     EXPECT_EQ(analyzer->popQuotes(test), "\"echo hello");    
+}
+
+TEST(ParserTest, MirrorQueue){
+    std::queue<Command*> cmdQ;
+    char* exec = "echo";
+    char* argsA = "A";
+    char* argsB = "B";
+    char* argsC = "C";
+    char* argsD = "D";
+    Command* lpar = new Connector("(");
+    Command* acmd = new BasicCommand(exec, argsA);
+    Command* andc = new Connector("&&");
+    Command* bcmd = new BasicCommand(exec, argsB);
+    Command* rpar = new Connector(")");
+    Command* orc = new Connector("||");
+    Command* ccmd = new BasicCommand(exec, argsC);
+    Command* dcmd = new BasicCommand(exec, argsD);  
+
+    cmdQ.push(lpar);
+    cmdQ.push(acmd);
+    cmdQ.push(andc);
+    cmdQ.push(bcmd);
+    cmdQ.push(rpar);
+    cmdQ.push(orc);
+    cmdQ.push(lpar);
+    cmdQ.push(ccmd);
+    cmdQ.push(andc);
+    cmdQ.push(dcmd);
+    cmdQ.push(rpar);
+
+    std::string test = "\"echo hello";
+    Parser* analyzer = new Parser("null");
+    std::queue<Command*> newQ = analyzer->mirror(cmdQ);
+    std::queue<Command*> syQ = analyzer->shuntingYard(newQ);
+
+    analyzer->buildTree(syQ);
+    //EXPECT_EQ(analyzer->popQuotes(test), "\"echo hello");    
+}
+
+TEST(ParserTest, PrescTest){
+    std::string test = "((echo hello && echo world) || (echo goodbye && echo earth))";
+    Parser* analyzer = new Parser(test);
+    analyzer->run();
+    //EXPECT_EQ(analyzer->popQuotes(test), "\"echo hello");    
 }
 
 #endif //__PARSER_TEST_HPP__

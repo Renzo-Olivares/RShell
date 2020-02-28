7e9bf6f391dc36df12b26fdd9e93f7aea56762db
Renzo <rmolivares@renzo-olivares.dev>
2020-02-27 23:46:43 -0800

    More leftover code

--- header/Command.hpp
+++ header/Command.hpp
@@ -11,8 +11,6 @@ class Command{
         virtual std::string cmdString() = 0;
         virtual char* getPath() = 0;
         virtual char** getArgs() = 0;
-        virtual void addCmd(Command* cmd){};
-        virtual void addQueue(std::queue<Command*> cmdQ){};
         virtual std::vector<char*> getRawCmd(){};
 };
 
